a9ca32eb004e25dbe65067d1b799d9e0ebacb729
devalex <beatoalex936@gmail.com>
2026-07-31 08:16:48 -0300

    link para whatzap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
   title: {
     default: SITE_NAME,
-    template: %s | ${SITE_NAME},
+    template:`%s | ${SITE_NAME}`,
   },
   description: SITE_DESCRIPTION,
   keywords: ['pet shop', 'ração', 'brinquedos', 'acessórios', 'pet'],
