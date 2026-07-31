8597ef66fb07ab45d7d357b180013f87edb9793b
devalex <beatoalex936@gmail.com>
2026-07-31 08:25:04 -0300

    colocando a imagem no link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     type: 'website',
     images: [
       {
-        url: '/banner-v2.jpg', // <--- Foto da loja (coloque em public/banner-v2.jpg)
+        url: '/link.jpg', // <--- Foto da loja (coloque em public/banner-v2.jpg)
         width: 1200,
         height: 630,
         alt: SITE_NAME,
